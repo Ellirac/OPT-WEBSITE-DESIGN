a412b553a258cd24ba7750e0910dfcbeef89ea14
CeaZapata <zapatacarille13@gmail.com>
2026-03-10 08:57:13 +0800

    Fixed unused UsedNavigate from Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import { Link, useNavigate } from "react-router-dom";
+import { Link, } from "react-router-dom";
 import { useRef } from "react";
 import logo from "../assets/images/opt_logo2.png";
 
